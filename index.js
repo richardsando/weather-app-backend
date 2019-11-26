0beb1a842078ebbf11a6d87eab73c07ee7a7d4d4
Richard Sando <richardsando369@gmail.com>
2019-11-26 14:18:51 +1100

    created git repo

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+const axios = require('axios')
+const dotenv = require('dotenv')
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+app.use(express.json())
+app.use(cors())
+
+const key = process.env.APIKEY
+app.get('/api', async (req, res) => {
+    try {
+        const latitude = req.headers.latitude
+        const longitude = req.headers.longitude
+        // const dummyURI = "https://api.darksky.net/forecast/308c7509067adff6de723594f5692940/37.8267,-122.4233?units=ca"
+        const response = await axios.get((`https://api.darksky.net/forecast/${key}/${latitude},${longitude}?units=ca`))
+        const data = response.data
+        res.send(data)
+    } catch (err) {
+        console.log(err)
+        res.status(500).send(err)
+    }
+})
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`Express listening on port ${port}!`))
